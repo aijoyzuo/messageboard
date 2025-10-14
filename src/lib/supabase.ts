4a71bb6a5f26382062d981ca55f72bf3a157e8ce
aijoyzuo <cctaijoy@gmail.com>
2025-10-14 11:36:14 +0800

    說明這次改了policy

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,9 +1,20 @@
-// lib/supabase.ts
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.SUPABASE_URL!;
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+async function testSupabase() {
+  const { data, error } = await supabase.from("Post").select("*").limit(5);
+  
+  if (error) {
+    console.error("Supabase error:", error);
+  } else {
+    console.log("Fetched data:", data);
+  }
+}
+
+testSupabase();
 
 export default supabase;
